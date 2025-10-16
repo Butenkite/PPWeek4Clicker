3ecba225e5b2f16c4d27491e87419a7212b368a1
Niko_Butenko <174657090+Butenkite@users.noreply.github.com>
2025-10-16 13:30:26 -0700

    cookie clicker

--- src/components/clickerGame.jsx
+++ src/components/clickerGame.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import cookieImage from '../assets/cookie.png'
 
 export default function ClickerGame(){
     let [count, setCount] = React.useState(0);
     return(
         <div className="cookieHolder">
             <button onClick={() => setCount((count) => count + 1)}>
-            Total Cookies: {count}
+            <img src = {cookieImage}/>
             </button>
+            <p>Total Cookies: {count}</p>
         </div>
     )
 }
