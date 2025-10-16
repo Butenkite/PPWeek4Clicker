9cd172fd5a5ccf24b875e2eca0e0eaa4f276e663
Niko_Butenko <174657090+Butenkite@users.noreply.github.com>
2025-10-16 14:15:58 -0700

    math logic

--- src/components/clickerGame.jsx
+++ src/components/clickerGame.jsx
@@ -4,14 +4,23 @@ import cookieImage from '../assets/cookie.png'
 export default function ClickerGame(){
     let [count, setCount] = React.useState(0);
     let [multiplier, SetMultiplier] = React.useState(1);
+    let [cost, SetCost] = React.useState(15);
 
     function increaseCount(){
         setCount((count) => count + multiplier)
     }
 
     function increaseMultiplier(){
-        SetMultiplier((multiplier) => multiplier + 1)
+        if(count > cost){
+            SetMultiplier((multiplier) => multiplier + 1)
+            increaseCost()
+        }
     }
+
+    function increaseCost(){
+        SetCost((cost => Math.floor(cost * (multiplier * 1.05 * 1.05))))
+    }
+
     return(
         <div className="cookieHolder">
             <button id = "clickerButton" class = "playButton" onClick={increaseCount}>
@@ -19,7 +28,7 @@ export default function ClickerGame(){
             </button>
             <p>Total Cookies: {count}</p>
             <button id = "multiplierButton" class = "playButton" onClick={increaseMultiplier}>
-            <p>multiplier button: {multiplier}</p>
+            <p>Cost to upgrade: {cost}</p>
             </button>
         </div>
     )
