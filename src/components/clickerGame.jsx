aba6f5f8b0475b96d11054bf7f90a4fecd46fc3b
JesseChum <jessechum@icloud.com>
2025-10-16 13:51:44 -0700

    Added multiplier button

--- src/components/clickerGame.jsx
+++ src/components/clickerGame.jsx
@@ -3,12 +3,16 @@ import cookieImage from '../assets/cookie.png'
 
 export default function ClickerGame(){
     let [count, setCount] = React.useState(0);
+    let [multiplier, SetMultiplier] = React.useState(1);
     return(
         <div className="cookieHolder">
             <button onClick={() => setCount((count) => count + 1)}>
             <img src = {cookieImage}/>
             </button>
             <p>Total Cookies: {count}</p>
+            <button onClick={() => SetMultiplier((multiplier) => multiplier + 1)}>
+            <p>multiplier button: {multiplier}</p>
+            </button>
         </div>
     )
 }
