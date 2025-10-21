7b9239f99bd34e16d3c04c887b7798bb51838421
JesseChum <jessechum@icloud.com>
2025-10-21 13:55:07 -0700

    fixed the image not showing up

--- src/components/CookieButton.jsx
+++ src/components/CookieButton.jsx
@@ -1,10 +1,10 @@
-import cookieImage from "../assests/cookie.png";
+import CookieImage from "../assets/cookie.png";
 
 export default function CookieButton({ onClick }){
     return(
         <div id="cookie">
             <button id="clickerButton" className="playButton" onClick={onClick}>
-                <img src={cookieImage} alt="cookie" />
+                <img src={CookieImage} alt="cookie" />
             </button>
         </div>
     )
